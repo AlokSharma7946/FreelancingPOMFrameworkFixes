46d2f6c29483e06c78d375c08a9198bd3e2e7052
alok <alokmani94@gmail.com>
2025-01-24 19:33:32 +0530

    Extent report changes to show result class wise

--- src/main/java/UtilitiesFactory/ExtentReportFactory.java
+++ src/main/java/UtilitiesFactory/ExtentReportFactory.java
@@ -1,23 +1,37 @@
 package UtilitiesFactory;
 
 import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.MediaEntityBuilder;
 import com.aventstack.extentreports.Status;
 import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 import com.aventstack.extentreports.reporter.configuration.Theme;
 import java.io.IOException;
+import java.lang.reflect.Method;
+
 import static UtilitiesFactory.EmailReportFactory.failed;
 import static UtilitiesFactory.EmailReportFactory.passed;
+
+
+
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.AfterMethod;
 
 
 
 public class ExtentReportFactory extends UtilFactory {
-
+    public ExtentReports extent; // Already defined
+    public ExtentTest scenarioDef; // Represents class-level nodes
+    public ExtentTest test;
     String fileName = reportLocation + "extentreport.html";
-
     public ExtentReportFactory() throws Exception {
+    }
+
+    public ExtentTest classLevelTest;
+
+    @BeforeMethod
+    public void startClassReport(Method method) {
+        classLevelTest = extent.createTest(method.getDeclaringClass().getSimpleName()); // This will create a node for the class
     }
 
     @BeforeMethod
@@ -45,21 +59,39 @@ public class ExtentReportFactory extends UtilFactory {
 //                        UtilFactory.getBase64Screenshot(BrowserFactory.getDriver(), "Screenshot")).build());
 //    }
 
+//    public void ExtentFailStep() throws IOException {
+//        failed++;
+//        String failureMessage = (failureException != null) ? failureException.replaceAll(",", "<br>") : "No exception message available";
+//
+//        // Ensure scenarioDef is initialized before logging
+//        if (scenarioDef != null) {
+//            scenarioDef.log(Status.FAIL,
+//                    "<details>" + "<summary> <b> <font color=red> Cause of Failure: </b> "
+//                            + "</font>" + "</summary>"
+//                            + failureMessage + "</details>", MediaEntityBuilder.createScreenCaptureFromBase64String(
+//                            UtilFactory.getBase64Screenshot(BrowserFactory.getDriver(), "Screenshot")).build());
+//        } else {
+//            System.out.println("scenarioDef is not initialized");
+//        }
+//    }
+
+    // Modify the method to pass a name for the screenshot
     public void ExtentFailStep() throws IOException {
         failed++;
+
         String failureMessage = (failureException != null) ? failureException.replaceAll(",", "<br>") : "No exception message available";
 
-        // Ensure scenarioDef is initialized before logging
-        if (scenarioDef != null) {
-            scenarioDef.log(Status.FAIL,
-                    "<details>" + "<summary> <b> <font color=red> Cause of Failure: </b> "
-                            + "</font>" + "</summary>"
-                            + failureMessage + "</details>", MediaEntityBuilder.createScreenCaptureFromBase64String(
-                            UtilFactory.getBase64Screenshot(BrowserFactory.getDriver(), "Screenshot")).build());
+        // Ensure test node is initialized
+        if (classLevelTest != null) {
+            // Use Base64 encoded screenshot and provide a name for the screenshot
+            String base64Screenshot = UtilFactory.getBase64Screenshot(BrowserFactory.getDriver(),"Failure Screenshot");
+            classLevelTest.fail("Test Failed: " + failureMessage,
+                    MediaEntityBuilder.createScreenCaptureFromBase64String(base64Screenshot).build());
         } else {
-            System.out.println("scenarioDef is not initialized");
+            System.out.println("Test node is not initialized for logging failure.");
         }
     }
+
 
 //    public void ExtentPassStep() throws IOException {
 //        passed++;
@@ -72,15 +104,17 @@ public class ExtentReportFactory extends UtilFactory {
 
     public void ExtentPassStep() throws IOException {
         passed++;
-        // Ensure scenarioDef is initialized before logging
-        if (scenarioDef != null) {
-            scenarioDef.log(Status.PASS,
-                    "<summary> <b> <font color=green> Test Passed: </b> "
-                            + "</font>" + "</summary>"
-                    , MediaEntityBuilder.createScreenCaptureFromBase64String(
-                            UtilFactory.getBase64Screenshot(BrowserFactory.getDriver(), "Screenshot")).build());
+
+        // Ensure the test node is initialized for logging
+        if (classLevelTest != null) {
+            // Get the Base64 screenshot string
+            String base64Screenshot = UtilFactory.getBase64Screenshot(BrowserFactory.getDriver(), "Pass Screenshot");
+
+            // Log the success with the screenshot attached
+            classLevelTest.pass("Test Passed",
+                    MediaEntityBuilder.createScreenCaptureFromBase64String(base64Screenshot).build());
         } else {
-            System.out.println("scenarioDef is not initialized");
+            System.out.println("Test node is not initialized for logging pass.");
         }
     }
 
--- src/test/java/Runner/TestRunnerListener.java
+++ src/test/java/Runner/TestRunnerListener.java
@@ -3,7 +3,13 @@ package Runner;
 import UtilitiesFactory.BrowserFactory;
 import UtilitiesFactory.EmailReportFactory;
 import UtilitiesFactory.ExtentReportFactory;
+import com.aventstack.extentreports.MediaEntityBuilder;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.testng.*;
+
+import java.io.File;
 import java.io.IOException;
 import static UtilitiesFactory.BrowserFactory.getDriver;
 import static UtilitiesFactory.UtilFactory.features;
@@ -23,16 +29,45 @@ public class TestRunnerListener implements ITestListener,IExecutionListener {
         extentReport.ExtentReport();
     }
 
+//    @Override
+//    public void onTestStart(ITestResult iTestResult) {
+//        // Creating child nodes for individual test methods
+//        String methodName = iTestResult.getMethod().getMethodName();
+//        extentReport.test = features.createNode(methodName);
+//        browserFactoryInstance.setBrowser(getParameterValue("browser"));
+//        emailReporting = getParameterValue("emailReport");
+//  //      emailRecipients = getParameterValue("emailRecipients");
+//
+//    }
+
     @Override
     public void onTestStart(ITestResult iTestResult) {
-        browserFactoryInstance.setBrowser(getParameterValue("browser"));
-        emailReporting = getParameterValue("emailReport");
-  //      emailRecipients = getParameterValue("emailRecipients");
+        // Get the class name of the test method
+        String className = iTestResult.getTestClass().getName();
+
+        // Check if the class node already exists, if not, create it
+        if (features == null || !features.getModel().getName().equals(className)) {
+            features = extentReport.extent.createTest(className);  // Create a new test for the class
+        }
+
+        // Create the method node under the class node
+        String methodName = iTestResult.getMethod().getMethodName();
+        extentReport.test = features.createNode(methodName);  // Method under the class node
+    }
+
+    private void createClassNode(ITestResult iTestResult) {
+        if (features == null) {
+            String className = iTestResult.getTestClass().getName();
+            features = extentReport.extent.createTest(className);
+        }
     }
 
     @Override
     public void onTestSuccess(ITestResult iTestResult) {
         try{
+            String screenshotPath = captureScreenshot(iTestResult.getMethod().getMethodName());
+            extentReport.test.pass("Test Passed: " +
+                    MediaEntityBuilder.createScreenCaptureFromPath(screenshotPath).build());
             extentReport.ExtentPassStep();
 //            getDriver().close();
 //            getDriver().quit();
@@ -44,17 +79,31 @@ public class TestRunnerListener implements ITestListener,IExecutionListener {
     @Override
     public void onTestFailure(ITestResult iTestResult) {
         try{
-            extentReport.ExtentFailStep();
+            String screenshotPath = captureScreenshot(iTestResult.getMethod().getMethodName());
+            extentReport.test.fail("Test Failed: " + iTestResult.getThrowable().getMessage(),
+                    MediaEntityBuilder.createScreenCaptureFromPath(screenshotPath).build());
             getDriver().close();
+            extentReport.ExtentFailStep();
         }catch (IOException e){
             e.printStackTrace();
         }
     }
 
+    private String captureScreenshot(String methodName) throws IOException {
+        TakesScreenshot ts = (TakesScreenshot) getDriver();
+        File source = ts.getScreenshotAs(OutputType.FILE);
+        String destination = System.getProperty("user.dir") + "/screenshots/" + methodName + ".png";
+        FileUtils.copyFile(source, new File(destination));
+        return destination;
+    }
+
     @Override
     public void onStart(ITestContext iTestContext) {
      //   scenarioDef = extentReport.extent.createTest(iTestContext.getName());
-        features = extentReport.extent.createTest("Google Search");
+//        features = extentReport.extent.createTest("Google Search");
+        // Creating a parent node in the Extent Report based on the test class name
+        String className = iTestContext.getAllTestMethods()[0].getRealClass().getSimpleName();
+        features = extentReport.extent.createTest(className);
     }
 
     @Override
